c014b6039c5d2ab399a5a41e597a1000b3fe7570
ChiaraDeNigris <c.denigris@studenti.unipi.it>
2021-04-10 12:40:53 +0200

    2 files modified

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var cityElems = Array.from(document.getElementsByClassName("citta"));
 for (let elem of cityElems) {
   elem.onclick = () => display(elem.innerHTML);
 }
-document.getElementById("calcoloMedia").onclick = () => media();
+document.getElementById("calcoloMassimo").onclick = () => massimo();
 
 function doCity(city, callback) {
   let promise = fetch(URL + city)
@@ -23,23 +23,12 @@ async function display(city) {
   document.getElementById("risposta").innerHTML =
     "A " + city + " ci sono " + t + " gradi";
 }
-async function media() {
+async function massimo() {
   let temps = await Promise.all(
     cityElems.map(cityElem =>
       doCity(cityElem.innerHTML, data => data.main.temp_max)
     )
   );
-  //let somma = temps.reduce((somma, temp) => temp + somma, 0);
-
   var massimo = temps.reduce((maxx, temp) => Math.max(temp, maxx));
-  document.getElementById("media").innerText = massimo;
+  document.getElementById("massimo").innerText = massimo;
 }
-/*
-async function media() {
-  let somma = 0;
-  for (let city of cityElems) {
-    somma += await doCity(city.innerHTML, data => data.main.temp);
-  }
-document.getElementById("media").innerText = somma/cityElems.length;
-}
-*/
